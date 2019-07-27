d940daf2dcf52416f754aa603741be6e39488087
Latch Kostov <latch.kostov@gmail.com>
2019-07-27 16:08:07 -0500

    Updates

---
+++ precache-manifest.ea6b4adc50fd375df59502a58e352122.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41068f0a7e96905082202df6e5270254",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "7d3b6abb3b313218fad3",
+    "url": "/monsters-roledex/static/css/main.e75ca511.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-roledex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7d3b6abb3b313218fad3",
+    "url": "/monsters-roledex/static/js/main.0d1ac97f.chunk.js"
+  },
+  {
+    "revision": "6a42e84361a161b4708c",
+    "url": "/monsters-roledex/static/js/runtime~main.dfc14819.js"
+  }
+]);
